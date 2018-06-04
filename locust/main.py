433d67d918ac4b762df8272aa6f6e6b3a0b9e6c4
g.punter <g.punter@bboxx.co.uk>
2018-06-04 12:32:57 +0100

    Remove import TaskSet

--- locust/main.py
+++ locust/main.py
@@ -12,7 +12,7 @@ import locust
 
 from . import events, runners, web
 from .parser import parse_options, create_options
-from .core import HttpLocust, Locust, TaskSet
+from .core import HttpLocust, Locust
 from .inspectlocust import get_task_ratio_dict, print_task_ratio
 from .log import console_logger, setup_logging
 from .runners import LocalLocustRunner, MasterLocustRunner, SlaveLocustRunner
