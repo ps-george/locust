47788a23731dad01b3622f4669cf9952b31c292a
g.punter <g.punter@bboxx.co.uk>
2018-06-05 12:19:34 +0100

    Change version and name for releasing my own package

--- locust/__init__.py
+++ locust/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 from .core import HttpLocust, Locust, TaskSet, task
 from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
